5a4d0ee4464f2842c307984a144238cb8c133f66
Kushal.Bhatt@asu.edu <Kushal.Bhatt@asu.edu>
2018-04-27 13:19:02 -0700

    VBR completed

--- src/Task2.java
+++ src/Task2.java
@@ -14,7 +14,7 @@ public class Task2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String inputfilepath = "K:/ASU/469_ Forensics/usb_test_raw.001";
+		String inputfilepath = args[0];//"K:/ASU/469_ Forensics/usb_test_raw.001";
 		String inputfilename = inputfilepath;
 		
 		//extract just the file name
@@ -39,7 +39,7 @@ public class Task2 {
 //	            
 //	            //Write this to file
 //	            String MD5Filename = "MD5-"+inputfilename+".txt";
-//	            writeDIgestToFile(MD5Filename, sb.toString());
+//	            writeDigestToFile(MD5Filename, sb.toString());
 //	            
 //	            /***************SHA-1*************/
 //	            byte sha_1[] = computeHash(file,"SHA-1");
@@ -53,7 +53,7 @@ public class Task2 {
 //	            
 //	          //Write this to file
 //	            String SHA1Filename = "SHA1-"+inputfilename+".txt";
-//	            writeDIgestToFile(SHA1Filename, sb.toString());
+//	            writeDigestToFile(SHA1Filename, sb.toString());
 //	            
 	            
 	            /** read mbr into buffer 
@@ -64,12 +64,12 @@ public class Task2 {
 	            byte[] mbrBytes = new byte[512];
 	           
 	            imageStream.read(mbrBytes);
+	            
 	            //for( int i = 0; i < mbrBytes.length; i++ )
 	            //	System.out.println(mbrBytes[i]); 
 	            imageStream.close();
-	            readMBR(mbrBytes);
-	            
-
+	            readMBR(mbrBytes,file);
+            
 	        } catch( IOException ioe ) {
 	            System.out.println( "Problem: " + ioe );
 	        }
@@ -77,13 +77,14 @@ public class Task2 {
 	}
 
 	//type, start sector address, and size of each partition in decimal should be printed
-	private static void readMBR(byte[] mbrBytes) {
+	private static void readMBR(byte[] mbrBytes,File file) {
 		/*
 		 * The partition entries start at offset 0x01BE	==  446 decimal. And each entry is 16 bytes long
 		 * type = offset 4;    
 		 * start sector = offset 8  (4 bytes-- little endian);  
 		 * size: offset 12   (4 bytes-- little endian)      
 		 */
+		
 		System.out.println( "Partition Entries:: ");
 		
 		byte partition[][] = new byte[4][16];
@@ -110,22 +111,93 @@ public class Task2 {
 	        for (byte b : partition[i]) {
 	            sb.append(String.format("%02X ", b));
 	        }
-	        System.out.println("\nPartition "+(i+1)+":=  "+ sb.toString());
+	        System.out.println("\n===============================================\nPartition "+(i+1)+":=  "+ sb.toString());
 	        String part[]=sb.toString().split(" ");
 	        
 	        //type
-	        System.out.print("("+part[4]+") "+getpartitionType(part[4])+", ");
+	        String type = getpartitionType(part[4]);
+	        System.out.print("("+part[4]+") "+type+", ");
 	        
 	        //start: byte 11 10 9 8
 	        int start = Integer.parseInt((part[11]+part[10]+part[9]+part[8]),16);
 	        System.out.print(start+", ");
 	        
 	        //size: byte 15 14 13 12
-	        int size = Integer.parseInt((part[15]+part[14]+part[13]+part[12]),16);
+	        long size = Long.parseLong((part[15]+part[14]+part[13]+part[12]),16);
 	        System.out.println(size);
 	        
+	        /****  TODO:// If partition is FAT-16 or FAT-32 then read VBR
+	         * 		(partition start sector * 512) =  byte offset where VBR is located
+	         * 			Read the necessary entries from there  ***/
+	        if(type.equals("FAT-16") || type.equals("FAT-32"))
+	        {
+	        	System.out.println("\n\t----------VBR------------");
+	        	InputStream imageStream;
+				try {
+					imageStream = new FileInputStream( file );
+					//skip all bytes till VBR
+		        	long skipped = imageStream.skip(start*512);// sector size = 512 bytes
+		        	//System.out.println("Skipped == "+skipped+"bytes");
+		        	byte[] vbrBytes = new byte[40];
+		            imageStream.read(vbrBytes);
+		            imageStream.close();
+		            sb= new StringBuilder();
+		            for (byte b : vbrBytes) {
+			            sb.append(String.format("%02X ", b));
+			        }
+			        //System.out.println("\nVBR:: "+ sb.toString());
+		            
+			        String vbr[] = sb.toString().split(" ");
+		            
+		            int reserved_area = Integer.parseInt((vbr[15]+vbr[14]),16); //size, you know start sector already
+		            int sectors_per_cluster = Integer.parseInt((vbr[13]),16);
+		            int no_fats = Integer.parseInt((vbr[16]),16);
+		            int fat_size = Integer.parseInt((vbr[23]+vbr[22]),16);
+		            if(type.equals("FAT-32"))
+		            	fat_size = Integer.parseInt((vbr[39]+vbr[38]+vbr[37]+vbr[36]),16);
+		            long no_of_sectors_inFS = Long.parseLong((vbr[35]+vbr[34]+vbr[33]+vbr[32]),16);//same as size of parition in MBR
+			       
+		            System.out.println("\tReserved Area:: start sector: "+start+"  end sector: "+(start+reserved_area)+"  size: "+reserved_area);		            
+		            //FAT starts after Reserved area... and goes for fat_size in sectors
+		            long fat_start = start+reserved_area+1;
+		            System.out.println("\tFAT Area::  start= "+(fat_start)+" End: "+(fat_start+(no_fats*fat_size))+"\n\t# of FATs = "+no_fats+"\n\tsize of each fat = "+fat_size);
+		            
+		            /* TODO:// For FAT-32  
+		             * 			     Start of Cluster 2 is just after FAT
+		             		   For FAT-16
+		             		    	FAT end is followed by Root Directory entries  and then Cluster2
+		             		    Calculate accordingly
+		             */
+		            
+		            if(type.equals("FAT-32"))
+		            	System.out.println("\tCLuster 2 start:"+(fat_start+(no_fats*fat_size) ) );
+		            else
+		            {
+		            	/*
+		            	 * calculate root directory entries size in # of sector
+		            	 * (Maximum Root Directory Entries * 32) / Bytes per Sector-- Assume 512 in our case
+		            	 */
+		            	int max_root_dir_entries = Integer.parseInt((vbr[18]+vbr[17]),16);
+		            	System.out.println("Root directory entries == "+max_root_dir_entries);
+		            	System.out.println("\tCLuster 2 start:"+((fat_start+(no_fats*fat_size)+(max_root_dir_entries*32/512) ) )  );
+		            }
+		            	
+			            
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+	            
+	        }
 		}
+	
 	}
+
+	
 
 	private static String getpartitionType(String type) {
 		// TODO Auto-generated method stub
@@ -158,8 +230,8 @@ public class Task2 {
 		case "A9": return "NetBSD";
 		case "C7": return "Currupted NTFS";
 		case "EB": return "BeOS";
+		default: return "unallocated";
 		}
-		return null;
 	}
 
 	//hash_algo = MD5 or SHA-1
@@ -199,7 +271,7 @@ public class Task2 {
 		return null;
 	}
 	
-	private static void writeDIgestToFile(String filename,String digest)
+	private static void writeDigestToFile(String filename,String digest)
 	{
 		Writer writer;
 		BufferedWriter bufferedWriter;
